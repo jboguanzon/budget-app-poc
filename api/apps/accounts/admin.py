604d96a1887b4cb4752425036af7da51ba33daec
Angel Lacson <angel@bld.ai>
2024-11-30 01:25:56 +0800

    feat(api): create account model

---
+++ api/apps/accounts/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Account
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ["name", "type", "currency", "user"]
