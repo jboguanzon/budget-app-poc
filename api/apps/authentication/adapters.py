3918e8492c9bbcd1c5e6ba143654e56108eb2e29
Jiego Guanzon <jboguanzon@gmail.com>
2024-11-29 15:02:14 +0800

    feat(api): setup authentication

--- api/apps/authentication/adapters.py
+++ api/apps/authentication/adapters.py
@@ -1,5 +1,13 @@
 from allauth.account.adapter import DefaultAccountAdapter
+from django.conf import settings
 
 
 class AccountAdapter(DefaultAccountAdapter):
-    pass
+    def get_email_confirmation_url(self, request, emailconfirmation):
+        """Constructs the frontend email confirmation (activation) url.
+
+        Note that if you have architected your system such that email
+        confirmations are sent outside of the request context `request`
+        can be `None` here.
+        """
+        return f"{settings.WEB_APP_BASE_URL}/{settings.WEB_APP_VERIFY_EMAIL_PATH}?key={emailconfirmation.key}"
--- api/config/urls.py
+++ api/config/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("apps.authentication.urls")),
 ]
 
 if settings.DEBUG:
