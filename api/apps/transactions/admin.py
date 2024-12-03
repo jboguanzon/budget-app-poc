e5a04ec8e711179fce697377309cbffc9d970f2c
Jiego Guanzon <jboguanzon@gmail.com>
2024-12-03 18:47:51 +0800

    feat(api): add transaction model

---
+++ api/apps/transactions/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Transaction
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = [
+        "account",
+        "date",
+        "payee",
+        "category",
+        "amount",
+        "running_balance",
+        "cleared",
+    ]
